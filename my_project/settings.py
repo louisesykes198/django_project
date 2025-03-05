c5a0b7c0558acc65f4b8fd1baf92e5f3b23c7f01
louisesykes198 <louisesykes198@yahoo.com>
2025-03-05 11:25:47 +0000

    Add heruko to server

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--%ut7(t1%v2(1u$j)$am$2ukg5y1)1(4@jc(nd*8_8)l40qd7l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
